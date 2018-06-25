e1434c899498a98b1b84debe8d5f144aa64f6371
tor <student2ua@gmail.com>
2018-06-25 18:00:36 +0300

    koin  ref edit

--- src/main/java/com/tor/graphqlexemple/di/MainModule.kt
+++ src/main/java/com/tor/graphqlexemple/di/MainModule.kt
@@ -3,9 +3,11 @@ package com.tor.graphqlexemple.di
 import com.tor.graphqlexemple.AppSchema
 import com.tor.graphqlexemple.dao.HumanStorageImp
 import com.tor.graphqlexemple.dao.IHumanStorage
+import org.jetbrains.squash.dialects.h2.H2Connection
 import org.koin.dsl.module.applicationContext
 
 val mainModule = applicationContext {
     provide { AppSchema(get()) }
-    provide { HumanStorageImp() as IHumanStorage }
+    provide { HumanStorageImp(get()) as IHumanStorage }
+    provide { H2Connection.createMemoryConnection() /*as DatabaseConnection*/ }
 }
