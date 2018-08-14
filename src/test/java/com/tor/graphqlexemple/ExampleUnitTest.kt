898b527bb2184e77c3a1e6c6b149a7b7269e517b
tor <student2ua@gmail.com>
2018-08-14 14:31:21 +0300

    fix err

--- src/test/java/com/tor/graphqlexemple/ExampleUnitTest.kt
+++ src/test/java/com/tor/graphqlexemple/ExampleUnitTest.kt
@@ -1,6 +1,6 @@
 package com.tor.graphqlexemple
 
-import com.tor.graphqlexemple.dao.ifNull
+import com.tor.graphqlexemple.dao.toNotNULL
 import com.tor.graphqlexemple.model.Human
 import org.junit.Assert.assertTrue
 import org.junit.Test
@@ -40,7 +40,7 @@ class ExampleUnitTest {
         var row = toTypedArray;
         val human = Human(
                 id = row[0].toInt(),
-                inn = row[1].ifNull(null),
+                inn = row[1],
                 lastName = row[2],
                 firsName = row[4],
                 middleName = row[5],
@@ -48,17 +48,17 @@ class ExampleUnitTest {
                 birthday = LocalDate.parse(row[11].trim('"'), DateTimeFormatter.ofPattern("yyyy-MM-dd 00:00:00")), //"2004-08-14 00:00:00"
                 brothersCount = row[14].toInt(),
                 childrenCount = row[15].toInt(),
-                sportText = row[16].ifNull("")!!,
-                amateurText = row[17].ifNull("")!!,
-                phone = row[25].ifNull("")!!,
+                sportText = row[16].toNotNULL(),
+                amateurText = row[17].toNotNULL(),
+                phone = row[25].toNotNULL(),
                 needHospice = row[27].toBoolean(),
                 isUkrainian = !row[29].toBoolean(),
-                email = row[36].ifNull("")!!,
-                remark = row[38].ifNull("")!!,
+                email = row[36].toNotNULL(),
+                remark = row[38].toNotNULL(),
                 isHumanDismissed = row[39].toBoolean(),
-                personCodeU = row[40].ifNull("")!!,
+                personCodeU = row[40].toNotNULL(),
                 idPerson = row[41].toIntOrNull(),
-                email2 = row[42].ifNull("")!!,
+                email2 = row[42].toNotNULL(),
                 experienceBegin = if (row[43].equals("NULL")) {
                     null
                 } else LocalDate.parse(row[43])
